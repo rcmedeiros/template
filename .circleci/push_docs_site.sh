d88d8e2e78548abd507163f97eb7700384ace144
Rafael Medeiros <rafael@rafaelmedeiros.eti.br>
2019-03-10 01:03:53 -0300

    still need to identify

--- .circleci/push_docs_site.sh
+++ .circleci/push_docs_site.sh
@@ -10,8 +10,9 @@ if [[ changes -ge 1 ]];
 then
     echo "${changes} changes to push"
     git add --all docs/
-#    git commit -m "Automated docs site generation build ${CIRCLE_BUILD_NUM}"
-    git commit -m "Flip theme to provoke massive update on automated build"
+    git config --global user.email "circleci@rafaelmedeiros.eti.br"
+    git config --global user.name "CircleCI"
+    git commit -m "Automated docs site generation build ${CIRCLE_BUILD_NUM}"
     git push
 else
     echo "No changes to push"
